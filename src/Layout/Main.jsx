9373d61eef6a5372c227670698bd29be3f97255a
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-05-10 20:30:45 +0600

    69 class 2 added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/Footer/Footer/Footer';
+import Navbar from '../pages/shared/Footer/Footer/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
+import { RouterProvider } from "react-router-dom";
+import router from "./Routs/Routs";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+    ,
+  </div>
+);
